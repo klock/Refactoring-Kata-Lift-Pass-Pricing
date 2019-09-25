542db9a665aa0c1f48a306d43d908792d7e5c601
Math Klock <math.klock@gmail.com>
2019-09-25 10:54:20 +0200

    [TEST] extract a function querying the application

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -8,42 +8,57 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
 import spark.Spark;
 
-public class PricesTest {
+class PricesTest {
 
     private Connection connection;
 
     @BeforeEach
-    public void createPrices() throws SQLException {
+    void createPrices() throws SQLException {
         connection = Prices.createApp();
     }
 
     @AfterEach
-    public void stopApplication() throws SQLException {
+    void stopApplication() throws SQLException {
         Spark.stop();
         connection.close();
     }
 
     @Test
-    public void doesSomething() {
-        JsonPath response = RestAssured.
-            given().
-                port(4567).
-            when().
-                param("type", "1jour").
-                get("/prices").
-            then().
-                assertThat().
-                    statusCode(200).
-                assertThat().
-                    contentType("application/json").
-            extract().jsonPath();
+    void priceForType1jour() {
+        JsonPath response = queryApplication(null, "1jour");
 
         assertEquals(35, response.getInt("cost"));
     }
 
+    private JsonPath queryApplication(String age, String type) {
+        RequestSpecification when = RestAssured
+                .given().port(4567)
+                .when();
+
+        // Add query parameter 'age' if present
+        if (StringUtils.isNotBlank(age)) {
+            when = when.param("age", age);
+        }
+        // Add query parameter 'type' if present
+        if (StringUtils.isNotBlank(type)) {
+            when = when.param("type", type);
+        }
+
+        return
+                when
+                        .get("/prices")
+                .then()
+                        .assertThat()
+                            .statusCode(200)
+                        .assertThat()
+                            .contentType("application/json")
+                        .extract().jsonPath();
+    }
 }
