90183d05a570a550d91dabddbd0bee54fdccbe39
Math Klock <math.klock@gmail.com>
2019-09-25 11:25:15 +0200

    [TEST] cover all specific values of age and night

--- java/src/test/java/dojo/liftpasspricing/PricesTest.java
+++ java/src/test/java/dojo/liftpasspricing/PricesTest.java
@@ -4,10 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.commons.util.StringUtils;
 
 import io.restassured.RestAssured;
@@ -35,6 +39,77 @@ class PricesTest {
         JsonPath response = queryApplication(null, "1jour");
 
         assertEquals(35, response.getInt("cost"));
+    }
+
+    @Test
+    void priceForTypeNightAndAgeNull() {
+        JsonPath response = queryApplication(null, "night");
+
+        assertEquals(0, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "{index} Age is {0}")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5})
+    void priceForAgeBetween0And6(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "1jour");
+
+        assertEquals(0, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "Age is {0}")
+    @MethodSource("rangeFrom6To64")
+    void priceForTypeNightAndAgeBetween6And63(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "night");
+
+        assertEquals(19, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "Age is {0}")
+    @MethodSource("rangeFrom65To100")
+    void priceForTypeNightAndAgeGreaterThan5(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "night");
+
+        assertEquals(8, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "Age is {0}")
+    @MethodSource("rangeFrom6To14")
+    void priceForTypeDifferentFromNightAndAgeLessThan15(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "1jour");
+
+        assertEquals(25, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "Age is {0}")
+    @MethodSource("rangeFrom15To64")
+    void priceForTypeDifferentFromNightAndAgeBetween15And64(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "1jour");
+
+        assertEquals(35, response.getInt("cost"));
+    }
+
+    @ParameterizedTest(name = "Age is {0}")
+    @MethodSource("rangeFrom65To100")
+    void priceForTypeDifferentFromNightAndAgeGreaterThan64(int age) {
+        JsonPath response = queryApplication(String.valueOf(age), "1jour");
+
+        assertEquals(27, response.getInt("cost"));
+    }
+
+    private static IntStream rangeFrom6To64() {
+        return IntStream.range(6, 64);
+    }
+
+    private static IntStream rangeFrom15To64() {
+        return IntStream.range(15, 64);
+    }
+
+    private static IntStream rangeFrom65To100() {
+        return IntStream.range(65, 100);
+    }
+
+    private static IntStream rangeFrom6To14() {
+        return IntStream.range(6, 14);
     }
 
     private JsonPath queryApplication(String age, String type) {
